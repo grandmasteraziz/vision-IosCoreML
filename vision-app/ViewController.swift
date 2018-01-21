a38096ad22e17d54864e3beab426fb0f2746c34c
grandmasteraziz <grandmasteraziz@hotmail.com>
2018-01-21 21:18:47 +0300

    Initial Commit

---
+++ vision-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-app
+//
+//  Created by Aziz on 21/01/2018.
+//  Copyright © 2018 vision-coreml. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
