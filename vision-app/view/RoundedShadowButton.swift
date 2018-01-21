1d037465177987406046cbad7669a08ba1de0d93
grandmasteraziz <grandmasteraziz@hotmail.com>
2018-01-22 00:09:49 +0300

    coreml

---
+++ vision-app/view/RoundedShadowButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedShadowButton.swift
+//  vision-app
+//
+//  Created by Aziz on 21/01/2018.
+//  Copyright © 2018 vision-coreml. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ vision-app/view/RoundedShadowImageView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedShadowImageView.swift
+//  vision-app
+//
+//  Created by Aziz on 21/01/2018.
+//  Copyright © 2018 vision-coreml. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
